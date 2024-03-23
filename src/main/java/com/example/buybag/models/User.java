74b6aedfe835748fcc2da0594baa0697ec88ed39
saikalbatyrbekova <saikal.batyrbekova@alatoo.edu.kg>
2024-03-23 23:47:50 +0600

    UPDATE User.java

--- src/main/java/com/example/buybag/models/User.java
+++ src/main/java/com/example/buybag/models/User.java
@@ -1,4 +1,39 @@
 package com.example.buybag.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import javax.management.relation.Role;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "email", unique = true)
+    private String email;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "active")
+    private boolean active;
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "image_id")
+    private Image avatar;
+    @Column(name = "password", length = 1000)
+    private String password;
+    private Set<Role> roles = new HashSet<>();
+    private LocalDateTime dateOfCreated;
+
+    @PrePersist
+    private void init(){
+        dateOfCreated = LocalDateTime.now();
+    }
 }
