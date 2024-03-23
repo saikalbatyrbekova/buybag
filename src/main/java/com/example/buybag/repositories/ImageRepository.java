0a13bae4e6ce7017a1b85d6fb9837b6435e5cfe5
saikalbatyrbekova <saikal.batyrbekova@alatoo.edu.kg>
2024-03-24 00:03:29 +0600

    UPDATE ImageRepository.java

--- src/main/java/com/example/buybag/repositories/ImageRepository.java
+++ src/main/java/com/example/buybag/repositories/ImageRepository.java
@@ -1,4 +1,7 @@
 package com.example.buybag.repositories;
 
-public class ImageRepository {
+import com.example.buybag.models.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
 }
