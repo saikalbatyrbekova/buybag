16cabdc2dd0a7ec970d204a83cb069bc5f93055b
saikalbatyrbekova <saikal.batyrbekova@alatoo.edu.kg>
2024-03-24 11:04:29 +0600

    UPDATE UserController.java

--- src/main/java/com/example/buybag/controllers/UserController.java
+++ src/main/java/com/example/buybag/controllers/UserController.java
@@ -1,4 +1,14 @@
 package com.example.buybag.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class UserController {
+    @GetMapping("/registration")
+    public String registration(){
+        return "registration";
+    }
 }
