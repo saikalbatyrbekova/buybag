7ef6e07a334d29fb39c4aff71d5480775e91d751
saikalbatyrbekova <saikal.batyrbekova@alatoo.edu.kg>
2024-03-24 11:23:31 +0600

    UPDATE UserController.java

--- src/main/java/com/example/buybag/controllers/UserController.java
+++ src/main/java/com/example/buybag/controllers/UserController.java
@@ -1,14 +1,44 @@
 package com.example.buybag.controllers;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.buybag.models.User;
+import com.example.buybag.services.UserService;
 
 @Controller
-@RequiredArgsConstructor
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
     @GetMapping("/registration")
     public String registration(){
         return "registration";
+    }
+
+    @PostMapping("/registration")
+    public ModelAndView createUser(User user){
+        ModelAndView modelAndView = new ModelAndView();
+        if(!userService.createUser(user)){
+            modelAndView.addObject("errorMessage", "User with Email: " + user.getEmail() + " already exists");
+            modelAndView.setViewName("registration");
+        } else {
+            modelAndView.setViewName("redirect:/login");
+        }
+        return modelAndView;
+    }
+
+    @GetMapping("/hello")
+    public String securityUrl(){
+        return "hello";
     }
 }
